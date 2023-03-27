85ca41c196db22d9b560708380e23ceaa5be4783
glushkov <klepa_82@bk.ru>
2023-03-27 22:53:09 +0300

    add tasks 2

---
+++ task.2/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 2: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных индексах.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int a = ReadInt("Введите размерность массива -> ");
+int b = ReadInt("Введите минимальное значение массива -> ");
+int c = ReadInt("Введите максимальное значение массива -> ");
+
+int[] array = GetRandomArray(a, b, c);
+Console.WriteLine($"[{String.Join(",", array)}]");
+Console.WriteLine($"Cумма отрицательных индексов в массиве - > {FindElement(array)}");
+
+
+int FindElement(int[] array)
+{
+    int summ = 0;
+    for (int count = 1; count < array.Length; count += 2)
+    {
+        summ = summ + array[count];
+    }
+    return summ;
+}
+
+// -------------------------------Общий метод-------------------------------------------
+int ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// -------------------------------Общий метод-------------------------------------------
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return result;
+}
